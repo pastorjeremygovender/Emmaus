a3ee8f83791f3438b60b956ff5166ac7c09f586f
pastorjeremygovender <pastorjeremygovender@gmail.com>
2026-09-09 15:29:21 +0200

    Add native Android Daily Rhythm widget

--- artifacts/project-emmaus/android/app/src/main/java/za/co/emmaus/app/MainActivity.java
+++ artifacts/project-emmaus/android/app/src/main/java/za/co/emmaus/app/MainActivity.java
@@ -2,4 +2,10 @@ package za.co.emmaus.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        DailyRhythmWidgetProvider.refresh(this);
+    }
+}
